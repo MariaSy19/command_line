f82d8e052ce20f318f133066c21c2d7c08d691d1
MariaSy19 <101586605+MariaSy19@users.noreply.github.com>
2023-10-30 00:34:52 +0200

    edit

--- src/Terminal.java
+++ src/Terminal.java
@@ -10,45 +10,10 @@ public class Terminal
     {
         switch (ch)
         {
-            case "echo":
-                echo(parser.getArgs());
-                break;
-            case "pwd":
-                pwd(parser.getArgs());
-                break;
-            case "history":
-                history(parser.getArgs());
-                break;
-            case "ls":
-                ls(parser.getArgs());
-                break;
-            case "ls-r":
-                ls-r(parser.getArgs());
-                break;
-            case "cp":
-                cp(parser.getArgs());
-                break;
             case "cd":
                 cd(parser.getArgs());
                 break;
-            case "rm":
-                rm(parser.getArgs());
-                break;
-            case "cat":
-                cat(parser.getArgs());
-                break;
-            case ">":
-                outputRedirect(parser.getArgs());
-                break;
-            case ">>":
-                withAppendOutput(parser.getArgs());
-                break;
-            case "toch":
-                toch(parser.getArgs());
-                break;
-            case "mkdir":
-                mkdir(parser.getArgs());
-                break;
+
             default:
                 System.out.println("command not found");
 
