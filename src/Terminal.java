dc0a4ce273734d8bef3adf6dc37c71502ef0c6ee
MariaSy19 <101586605+MariaSy19@users.noreply.github.com>
2023-10-29 22:36:55 +0200

    added function
    chooseCommandAction
    in class terminal

--- src/Terminal.java
+++ src/Terminal.java
@@ -2,11 +2,58 @@ public class Terminal
 {
     Parser parser;
     //Implement each command in a method, for example:
-    public String pwd(){...}
-    public void cd(String[] args){...}
+    //public String pwd(){}
+    public void cd(String[] args){}
     // ...
 //This method will choose the suitable command method to be called
-    public void chooseCommandAction(){...}
-    public static void main(String[] args){...}
+    public void chooseCommandAction(String ch)
+    {
+        switch (ch)
+        {
+            case "echo":
+                echo(parser.getArgs());
+                break;
+            case "pwd":
+                pwd(parser.getArgs());
+                break;
+            case "history":
+                history(parser.getArgs());
+                break;
+            case "ls":
+                ls(parser.getArgs());
+                break;
+            case "ls-r":
+                ls-r(parser.getArgs());
+                break;
+            case "cp":
+                cp(parser.getArgs());
+                break;
+            case "cd":
+                cd(parser.getArgs());
+                break;
+            case "rm":
+                rm(parser.getArgs());
+                break;
+            case "cat":
+                cat(parser.getArgs());
+                break;
+            case ">":
+                outputRedirect(parser.getArgs());
+                break;
+            case ">>":
+                withAppendOutput(parser.getArgs());
+                break;
+            case "toch":
+                toch(parser.getArgs());
+                break;
+            case "mkdir":
+                mkdir(parser.getArgs());
+                break;
+            default:
+                System.out.println("command not found");
+
+        }
+    }
+    public static void main(String[] args){}
 
 }
