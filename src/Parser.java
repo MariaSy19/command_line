c8333aaec825f414ff2dc0dbc6cce1519f2a0fed
MariaSy19 <101586605+MariaSy19@users.noreply.github.com>
2023-10-30 19:28:15 +0200

    add function parse
    in class parser

--- src/Parser.java
+++ src/Parser.java
@@ -1,10 +1,31 @@
-public class Parser
-{
+class Parser {
     String commandName;
     String[] args;
-    //This method will divide the input into commandName and args
-     //where "input" is the string command entered by the user
-    //public boolean parse(String input){}
-    public String getCommandName(){return commandName;}
-    public String[] getArgs(){return args;}
+
+    // This method will divide the input into commandName and args
+    // where "input" is the string command entered by the user
+    public boolean parse(String input)
+    {
+        //split to get individual string
+        String[] str = input.split(" ");
+        //check if there are no strings
+        if (str.length == 0)
+        {
+            System.out.println("Invalid command! ");
+            return false; //parsing failure
+        }
+        commandName = str[0]; //first string assumed command name
+        args = new String[str.length - 1]; // The rest of the strings (if any) are considered as arguments
+        System.arraycopy(str, 1, args, 0, str.length - 1);// Copy the arguments from string into the args array
+
+        return true;//parsing successful
+    }
+
+    public String getCommandName() {
+        return commandName;
+    }
+
+    public String[] getArgs() {
+        return args;
+    }
 }
