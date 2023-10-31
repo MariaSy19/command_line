66f04d369c16bdb73ec19c12debe6edf64d8eb8a
mohamed_mahmoud <102570475+MohamedMahmoud01110@users.noreply.github.com>
2023-10-31 20:06:43 +0200

    Update Parser.java
     add conditions to check all invalid inputs

--- src/Parser.java
+++ src/Parser.java
@@ -17,7 +17,22 @@ class Parser {
         commandName = str[0]; //first string assumed command name
         args = new String[str.length - 1]; // The rest of the strings (if any) are considered as arguments
         System.arraycopy(str, 1, args, 0, str.length - 1);// Copy the arguments from string into the args array
-
+        if(( commandName.equals("pwd")) && args.length != 0)
+        {
+            System.out.println("ERROR: " + commandName + " not take any argumnent!");
+            return false;
+        }
+        if(( commandName.equals("mkdir") ||commandName.equals("cat") ) && args.length == 0 )
+        {
+            System.out.println("ERROR: " + commandName + " take at least 1 argument!");
+            return false;
+        }
+        if(( commandName.equals("echo") || commandName.equals("rmdir") ||commandName.equals("touch")
+            ||commandName.equals("rm") ) && args.length != 1 )
+        {
+            System.out.println("ERROR: " + commandName + " take one argument!");
+            return false;
+        }
         return true;//parsing successful
     }
 
